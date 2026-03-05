c42fbcf0f5434533678562aa4af656f09f3f6922
Yash Agrawal <yash@Yashs-MacBook-Air-310.local>
2026-03-05 12:44:01 -0500

    lua formatter

--- home/.config/nvim/lua/plugins/lsp-config.lua
+++ home/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,45 +1,61 @@
 local tools = {
-  -- LSP
-  'lua_ls',
-  'rust_analyzer',
-  'clangd',
-  'typescript-language-server',
-  'pyright',
-  'json-lsp',
-  -- Format
-  'clang-format',
-  'prettier'
-  -- Lint
+	-- LSP
+	"lua_ls",
+	"rust_analyzer",
+	"clangd",
+	"typescript-language-server",
+	"pyright",
+	"json-lsp",
+	-- Format
+	"clang-format",
+	"prettier",
+	"stylua",
+	-- Lint
 }
 
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {}
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = { ensure_installed = tools },
-    dependencies = { "mason-org/mason.nvim" },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Hover" })
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Goto definition"})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = "Goto references" })
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename symbol" })
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code Actions" })
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Line diagnostic" })
-      vim.keymap.set("n", "<leader>l", vim.lsp.buf.format, { desc = "Format" })
-    end
-  }
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = { ensure_installed = tools },
+		dependencies = { "mason-org/mason.nvim" },
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto definition" })
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto references" })
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Line diagnostic" })
+			vim.keymap.set("n", "<leader>l", vim.lsp.buf.format, { desc = "Format" })
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.clang_format,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.stylua,
+				},
+			})
+		end,
+	},
 }
